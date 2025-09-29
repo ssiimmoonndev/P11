ced58611caf21876c2140a8bd56bdb386ee824fd
Simon B <simon.blecot@wifirst.fr>
2025-09-29 15:56:27 +0200

    ErrorPage si URL pas bon

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,8 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import ReactDOM from 'react-dom/client'
-// import { BrowserRouter } from 'react-router-dom'
-// import './index.css'
-// import App from './App.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./index.css";
+import "./style/styles.scss"; // si tu veux garder ton SCSS, sinon commente
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
